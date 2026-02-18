c024f0bd12a691c70f2ecf065660fe2d378fcf9f
Ianis Triandafilov <janis.sci@gmail.com>
2026-02-18 14:46:39 +0100

    history: last message first

--- src/helpers/history.ts
+++ src/helpers/history.ts
@@ -6,7 +6,7 @@ const HISTORY_FILE = path.join(os.homedir(), ".nicechat_history");
 
 export function loadHistory(): string[] {
   try {
-    return fs.readFileSync(HISTORY_FILE, "utf-8").split("\n").filter(Boolean);
+    return fs.readFileSync(HISTORY_FILE, "utf-8").split("\n").filter(Boolean).reverse();
   } catch {
     return [];
   }
